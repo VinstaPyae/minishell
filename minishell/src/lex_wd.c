0490116d54022db648ab0e3f3df87a1e9788bb1e
Pyae Phyo Zaw <pzaw@c2r1s6.42singapore.sg>
2025-01-24 18:36:32 +0800

    only word with space

--- minishell/src/lex_wd.c
+++ minishell/src/lex_wd.c
@@ -53,35 +53,13 @@ int	lex_token_variable(char *str, int *i, t_list **l_token)
 	t_list	*token;
 	char	*str_token;
 	int		v_len;
-	int		space;
 
 	v_len = variable_len(&str[*i]);
-	space = 0;
 	if (v_len > 0)
 	{
-		if (space)
-        {
-            // Include one space after the variable
-            str_token = malloc(v_len + 2); // +1 for space, +1 for null terminator
-            if (str_token == NULL)
-            {
-                print_error(__func__, __FILE__, __LINE__, "Mem allocate failed for str_token");
-                return (1);
-            }
-            ft_strlcpy(str_token, &str[*i], v_len + 1); // Copy the variable
-            str_token[v_len] = '*'; // Add the space
-            str_token[v_len + 1] = '\0'; // Null-terminate
-        }
-        else
-        {
-            // No space after the variable
-            str_token = ft_substr(str, *i, v_len);
-            if (str_token == NULL)
-            {
-                print_error(__func__, __FILE__, __LINE__, "Mem allocate failed for str_token");
-                return (1);
-            }
-		}
+		str_token = ft_substr(str, *i, v_len);
+		if (!str_token)
+			return (free(str_token), 1);
 		token = create_token(str_token, TOKEN_VARIABLE);
 		if (!token)
 			return (free(str_token), ft_lstclear(&token, c_token_destroy), 1);
